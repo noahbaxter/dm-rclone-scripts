0f6c11a0d2ed35edb8d943e49aad0fe8de1dbe03
Noah Baxter <noahbaxt@gmail.com>
2024-07-17 18:09:03 -0700

    back to sync, but removing fast-lists (which seems to have been the problem), added new flags to maaaassively speedup sync times (checks in as little as 4min now)

--- MacOS/sync.sh
+++ MacOS/sync.sh
@@ -1,4 +1,4 @@
 local_path="$(dirname "$0")/Sync Charts"
-rclone copy "gdrive:CH Charts" "$local_path" -vv --fast-list
+rclone sync "gdrive:CH Charts" "$local_path" -vv --drive-pacer-min-sleep=10ms --drive-pacer-burst=200 --checkers=16
 echo "PRESS ENTER"
 read
--- Windows/sync.bat
+++ Windows/sync.bat
@@ -1,3 +1,3 @@
-.\rclone.exe copy "gdrive:CH Charts" "%~dp0/Sync Charts" -vv --fast-list
+.\rclone.exe sync "gdrive:CH Charts" "%~dp0/Sync Charts" -vv --drive-pacer-min-sleep=10ms --drive-pacer-burst=200 --checkers=16
 echo PRESS ENTER
 pause
